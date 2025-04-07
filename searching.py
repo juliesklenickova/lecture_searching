a678ca50c0de372e86e557b194403bf4fc98a7d8
juliesklenickova <266022@vutbr.cz>
2025-04-07 13:33:41 +0200

    linearni hledani, commit 2

--- searching.py
+++ searching.py
@@ -20,14 +20,23 @@ def read_data(file_name, field):
     else:
         vystup = None
     return vystup
-
+def linear_search(prohledavana_sekvence, hledane_cislo):
+    mezi_seznam = []
+    for idx, cislo in enumerate(prohledavana_sekvence):
+        if cislo == hledane_cislo:
+            mezi_seznam.append(idx)
+        else:
+            pass
+    dictionary_vystup = {"positions":mezi_seznam, "count":len(mezi_seznam)}
+    return dictionary_vystup
 
 
 def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
     sequential_data = read_data("sequential.json", "unordered_numbers")
     print(read_data("sequential.json", "unordered_numbers"))
+    found_numbers = linear_search(sequential_data, 0)
+    print(found_numbers)
+if __name__ == '__main__':
+    main()
+
+
